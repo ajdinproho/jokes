7e18dea8b0f11be9977a325e382f3452f94f09c6
Ajdin <ajdinproho@gmail.com>
2019-01-23 20:51:07 +0100

    update

---
+++ src/components/JokesApi.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="input-wrap">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'JokesApi',
+  data() {
+
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/store/index.js
@@ -0,0 +1,8 @@
+import actions from "./actions";
+import mutations from "./mutations";
+import getters  from "./getters";
+import state from "./state";
+
+export default {
+
+};
